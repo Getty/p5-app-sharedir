236a5bf3cc96ca4f9db74ba52911d95c6ca6edfe
Torsten Raudssus <torsten@raudss.us>
2013-09-24 06:07:00 +0200

    First commit, else I cant build

---
+++ lib/App/ShareDir.pm
@@ -0,0 +1,28 @@
+package App::ShareDir;
+# ABSTRACT: Applications for using File::ShareDir
+
+use strict;
+use warnings;
+
+1;
+
+=head1 SYNOPSIS
+
+  distdir File-ShareDir
+  moduledir File::ShareDir
+  distfile File-ShareDir file/name.txt
+  modulefile File::ShareDir file/name.txt
+  classfile Foo::Bar file/name.txt
+
+=head1 DESCRIPTION
+
+=head1 SUPPORT
+
+Repository
+
+  http://github.com/Getty/p5-app-sharedir
+  Pull request and additional contributors are welcome
+ 
+Issue Tracker
+
+  http://github.com/Getty/p5-app-sharedir/issues
